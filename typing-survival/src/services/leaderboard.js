0dc8226d4356db3563b6320a710e74fb4ea9e418
James I. Uriarte <j_uriarte@u.pacific.edu>
2026-04-23 01:19:53 -0700

    use env api url

--- typing-survival/src/services/leaderboard.js
+++ typing-survival/src/services/leaderboard.js
@@ -1,4 +1,5 @@
-const API_URL = "http://localhost:5000/api/leaderboard";
+const API_URL =
+    import.meta.env.VITE_API_URL || "http://localhost:5000/api/leaderboard";
 
 export async function getLeaderboard() {
     const response = await fetch(API_URL);
